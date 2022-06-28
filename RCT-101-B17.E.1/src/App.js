3c73082c316be19fce77d600c6c1e8805a86b0ce
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-06-29 02:27:30 +0530

    Add files via upload

---
+++ RCT-101-B17.E.1/src/App.js
@@ -0,0 +1,36 @@
+
+const container={ 
+
+
+  marginBottom: '50px',
+  marginLeft: '5%',
+  marginright:'5%',
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  function App() {
+    return (
+      <div>
+       <h1 >Slides</h1> 
+       <h2 >Intro to react</h2> 
+       <h2 >React is js ui libarary</h2> 
+       <button >next</button> 
+      
+                 
+                      
+                  
+                 
+      </div>
+    );
+  }
+  
+  export default App;
+
+
